4128b3d82d8979ad0f92d3f5024b47d873df740c
zhanghui <@ioszhanghui@163.com>
2018-05-21 18:56:49 +0800

    提交文件

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,92 +1,23 @@
 //index.js
 //获取应用实例
 const app = getApp()
-var commen =require("../../commen/commen.js");
-/*保存产生的点*/
-var lists = [];
-//px为单位的点
-var pxList = [];
-var count=1;//默认筛子的个数
-/*筛子对应的链接*/
-var saizi = ["http://p8x4grwe5.bkt.clouddn.com/SZ_01@2x.png",
-"http://p8x4grwe5.bkt.clouddn.com/SZ_02@2x.png",
-"http://p8x4grwe5.bkt.clouddn.com/SZ_03@2x.png",
-"http://p8x4grwe5.bkt.clouddn.com/SZ_04@2x.png",
-"http://p8x4grwe5.bkt.clouddn.com/SZ_05@2x.png",
-"http://p8x4grwe5.bkt.clouddn.com/SZ_06@2x.png"
-];
-
-/*筛子的可滚动区域*/
-function setSaiziScrollPoint() {
-  /*全是px*/
-  var screenwidth = app.globalData.screenWidth;//屏宽
-  var saiziWidth = 76;//筛子的大小
-  var margin = 10;
-  //最左边的位置
-  var minX = margin;
-  //最右边的位置
-  var maxX = screenwidth - margin - 76;
-  /*最小的*/
-  var minY = app.globalData.nviHight + 20 + margin;
-  /*最大的*/
-  var maxY = app.globalData.screenHeight - 55 - 64 - 20 - 76;
-  var point = {
-    left: Math.ceil(commen.randomValue(minX, maxX)),
-    top: Math.ceil(commen.randomValue(minY, maxY)),
-    imageUrl: saizi[Math.round(Math.random() * 5)],
-   };
-  return point;
-}
-/*查询数据是否重复*/
-function isRepeatData(point){
-  var repeat =false;
-  for (var i = 0; i < pxList.length;i++){
-    var leftPoint = pxList[i];
-    var offsetLeft = Math.abs(point.left-leftPoint.left);
-    var offsetTop = Math.abs(point.top-leftPoint.top);
-    console.log("**********"+"offsetLeft" + offsetLeft + "offsetTop" + offsetTop);
-    if (offsetLeft < (76 + 5) && offsetTop < 76+5 ){
-      console.log("重点");
-      repeat = true;
-      break;
-    }
-  }
-  return repeat;
-}
-
-/*产生筛子的点*/
-function addSaiziPoint(){
-
-  for (var i = 0; i < count;i++){
-    //产生一个随机点
-    var point = setSaiziScrollPoint();
-    while (isRepeatData(point)) {
-      point = setSaiziScrollPoint();
-    }
-    /*创建动画*/
-    var animation = wx.createAnimation({
-      duration: 2000,
-      timingFunction: 'linear',
-      delay: 3000
-    });
-    animation.translate(point.left,point.top).rotate(180).step();
-    point.animation = animation.export();
-    console.log("左边"+point.left+"顶部"+point.top);
-    pxList.push(point);
-    lists.push(point);
-  }
-}
-
+var commen =require("../../commen/commen.js")
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    count:count,
-    lists: lists,
-    saizi: saizi,
-    pxList: pxList
+    count:1,
+    lists: [],
+    saizi:["http://p8x4grwe5.bkt.clouddn.com/SZ_01@2x.png",
+      "http://p8x4grwe5.bkt.clouddn.com/SZ_02@2x.png",
+      "http://p8x4grwe5.bkt.clouddn.com/SZ_03@2x.png",
+      "http://p8x4grwe5.bkt.clouddn.com/SZ_04@2x.png",
+      "http://p8x4grwe5.bkt.clouddn.com/SZ_05@2x.png",
+      "http://p8x4grwe5.bkt.clouddn.com/SZ_06@2x.png"
+    ],
+    pxList:[]
   },
   onReady:function(){
     //创建音频加载对象
@@ -104,6 +35,64 @@ Page({
     })
 
   },
+  /*筛子的可滚动区域*/
+  setSaiziScrollPoint:function() {
+    /*全是px*/
+    var screenwidth = app.globalData.screenWidth;//屏宽
+    var saiziWidth = 76;//筛子的大小
+    var margin = 10;
+    //最左边的位置
+    var minX = margin;
+    //最右边的位置
+    var maxX = screenwidth - margin - 76;
+    /*最小的*/
+    var minY = app.globalData.nviHight + 20 + margin;
+    /*最大的*/
+    var maxY = app.globalData.screenHeight - 55 - 64 - 20 - 76;
+    var point = {
+      left: Math.ceil(commen.randomValue(minX, maxX)),
+      top: Math.ceil(commen.randomValue(minY, maxY)),
+      imageUrl: this.data.saizi[Math.round(Math.random() * 5)],
+    };
+    return point;
+  },
+  /*查询数据是否重复*/
+  isRepeatData:function(point){
+    var repeat = false;
+    for (var i = 0; i < this.data.pxList.length;i++){
+      var leftPoint = this.data.pxList[i];
+    var offsetLeft = Math.abs(point.left - leftPoint.left);
+    var offsetTop = Math.abs(point.top - leftPoint.top);
+    if (offsetLeft < (76 + 5) && offsetTop < 76 + 5) {
+      console.log("重点");
+      repeat = true;
+      break;
+    }
+}
+return repeat;
+},
+
+  /*产生筛子的点*/
+  addSaiziPoint:function(){
+    for (var i = 0; i < this.data.count;i++){
+  //产生一个随机点
+  var point = this.setSaiziScrollPoint();
+  while (this.isRepeatData(point)) {
+    point = this.setSaiziScrollPoint();
+  }
+  /*创建动画*/
+  var animation = wx.createAnimation({
+    duration: 2000,
+    timingFunction: 'linear',
+    delay: 3000
+  });
+  animation.translate(point.left, point.top).step().rotate(180).step();
+  point.animation = animation.export();
+  console.log("左边" + point.left + "顶部" + point.top);
+  this.data.pxList.push(point);
+  this.data.lists.push(point);
+}
+},
   /**
    * 生命周期函数--监听页面显示
    */
@@ -111,9 +100,6 @@ Page({
     /*加速剂摇晃事件*/
     var that =this;/***???????*/
     wx.onAccelerometerChange(function(res){
-      console.log(res.x+"x");
-      console.log(res.y+"y");
-      console.log(res.z+'z');
       if(res.x>0.3&&res.y>0.3){
         console.log("加速计摇晃");
         //创建音频加载对象
@@ -125,7 +111,7 @@ Page({
         pxList.length = 0;
         addSaiziPoint();
         that.setData({
-          lists: lists
+          lists: this.data
         });
       }
 
@@ -133,20 +119,20 @@ Page({
   },
   /*增加筛子的个数*/
   addNumber:function(){
-    if(count>=10){
+    if (this.data.count>=10){
       return;//最多10个
     }
     this.setData({
-      count:++count
+      count: ++this.data.count
     });
   },
   /*减少筛子的个数*/
   reduceNumber:function(){
-    if(count<=1){
+    if (this.data.count<=1){
       return;//最少一个
     }
     this.setData({
-      count: --count
+      count: --this.data.count
     });
   },
   /*设置*/
@@ -157,15 +143,17 @@ Page({
     this.audioObj.play();
 
     //获取坐标点
-    lists.length = 0;//清空数组
-    pxList.length = 0;
+    this.data.lists.length = 0;//清空数组
+    this.data.pxList.length = 0;
     //清除视图数据
     this.setData({
-      lists: lists
+      lists:[],
+      pxList:[]
     });
-    addSaiziPoint();
+    this.addSaiziPoint();
+    var that = this;
     this.setData({
-      lists: lists
+      lists: this.data.lists
     });
  }
 })
